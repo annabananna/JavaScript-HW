5f49085532ab79086f6daae855ba25a1be14a0b4
annabananna <ana_bogdanovska@yahoo.com>
2022-12-04 15:16:26 +0100

    Add files via upload

---
+++ JS HW01/app.js
@@ -0,0 +1,26 @@
+//Write a JavaScript program that will read the two sides of a rectangle and calculate its area
+//TIP: area = a * b
+
+
+let height = 2;
+let width = 4;
+let rectangleArea = height * width;
+console.log("The area of the rectangle is: ", rectangleArea);
+
+//Write a JavaScript program that will calculate area of Circle
+//TIP: area = π * r2
+
+let radius = 3;
+let pi = 3.14;
+let circleArea = pi * (radius)**2;
+console.log("The area of the circle is: ", circleArea);
+
+// Write a JavaScript program that will calculate the price of 30 Phones, where the price of one phone is $119.95 and the tax rate is 5%.
+
+let phonesNumber = 30;
+let price = 119.95;
+let taxRate = 5; // 5%
+
+let fullPrice = 30 * (119.5 + ((5/119.5)*100));
+console.log("The price for 30 phones is ", fullPrice, "$");
+
